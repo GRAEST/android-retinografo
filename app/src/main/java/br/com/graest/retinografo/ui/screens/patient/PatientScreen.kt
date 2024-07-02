d0b5bd785cedec228a543120384fc151254844e0
Marcus Macbook <marcuseduardo.ns@gmail.com>
2024-07-02 08:40:24 -0400

    Avoiding commit problems

--- app/src/main/java/br/com/graest/retinografo/ui/screens/patient/PatientScreen.kt
+++ app/src/main/java/br/com/graest/retinografo/ui/screens/patient/PatientScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -23,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
